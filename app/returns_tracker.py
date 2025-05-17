72b1af00e08102ca866ec4cae2a96191192ead08
dones97 <dones9798@gmail.com>
2025-05-16 22:04:26 -0400

    Update returns_tracker.py

--- app/returns_tracker.py
+++ app/returns_tracker.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 import streamlit as st
 
-ticker = "DODLA.BO"
+ticker = "543306.BO"
 data = yf.Ticker(ticker).history(period="1mo")
 st.write(f"Fetched data for {ticker}:", data)
